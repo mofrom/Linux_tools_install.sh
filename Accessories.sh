99375e008a8ae07c46e95fd135dcaa1637890c26
mofrom <914251253@qq.com>
2018-02-14 18:05:43 +0800

    update

--- Accessories.sh
+++ Accessories.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #附件
-
-#translate[Google translate API]
-echo "install Google translate............"
-echo ""
-sudo apt-get -y install translate-shell translate-toolkit  translate-toolkit-doc
-
+which trans
 if [[ $? -eq 0 ]]; then
-	echo -e "Successful installation  \033[32;40m√\033[0m"
-fi
+	echo -e "已经安装    \033[32;40m√\033[0m"
+else
+	#translate[Google translate API]
+	echo "install Google translate............"
+	echo ""
+	sudo apt-get -y install translate-shell translate-toolkit  translate-toolkit-doc
+
+	if [[ $? -eq 0 ]]; then
+		echo -e "Successful installation  \033[32;40m√\033[0m"
+	fi
 
 cat << EOF
 ---------------------------------------------------------------
@@ -16,3 +19,4 @@ cat << EOF
  	     https://github.com/soimort/translate-shell/wiki
 ---------------------------------------------------------------
 EOF
+fi
--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-Linux日常生活，code
+Linux日常生活，coding
 # 【待完善，请等待.....】
+PS:本脚本只用于Debian、Kali、~~Linux Mint~~ ,其他发行版请自行编写。
+
+小ps:各位如果有好建议(或者应用与终端工具)，请联系本人QQ:354096994。
+
 > 使用：
-   1. `git clone https://github.com/snowfall4/DailyLinux.git` 
-   2. `cd DailyLinux.git/`
+   1. `git clone https://github.com/YGoldking/DailyLinux.git` 
+   2. `cd DailyLinux/`
    3. `chmod +x install.sh`
    4. `./install.sh`
+   5. 更新本脚本运行：`./upgrade.sh`
 
+> wiki:
+  - https://github.com/YGoldking/DailyLinux/wiki/Document
